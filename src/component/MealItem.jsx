be3eb52e4f0cf71141eb1d06af9e1e65adc1eb15
ritacc12 <rita20920@gmail.com>
2024-03-08 16:39:06 +0800

    create meal Item component for menu

---
+++ src/component/MealItem.jsx
@@ -0,0 +1,20 @@
+import Card from "react-bootstrap/Card";
+
+export default function MealItem({ id, title, price, image }) {
+  return (
+    <>
+      <Card
+        style={{
+          width: "18rem",
+        }}
+        id={id}
+      >
+        <Card.Img variant="top" src={image} alt={title} className="rounded" />
+        <Card.Body>
+          <Card.Title className="mealTitle">{title}</Card.Title>
+          <Card.Text className="mealPrice">${price}</Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  );
+}
