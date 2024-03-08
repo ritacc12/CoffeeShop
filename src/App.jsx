2f4452bf7e69a0cb132cb98305d2f940e65b7724
ritacc12 <rita20920@gmail.com>
2024-03-08 16:53:44 +0800

    add BrowserRouter & set the route for page

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import "./App.css";
+import Layout from "./page/Layout.jsx";
+import Header from "./component/Header.jsx";
+import ShopInfo from "./page/ShopInfo.jsx";
+import News from "./page/News.jsx";
+import About from "./page/About.jsx";
+import Footer from "./component/Footer.jsx";
+import Menu from "./page/Menu.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+        <Route path="/news" element={<News />}></Route>
+        <Route path="/shop" element={<ShopInfo />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/menu" element={<Menu />}></Route>
+      </Routes>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
