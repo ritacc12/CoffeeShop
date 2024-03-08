1653584e7537edb736918a5060b3c489908d9f2c
ritacc12 <rita20920@gmail.com>
2024-03-08 16:00:58 +0800

    import the dummy menu data

---
+++ src/assets/drink.js
@@ -0,0 +1,31 @@
+import blackTea from "/pictures/blacktea.jpg";
+import Juice from "/pictures/juice.jpg";
+import Latte from "/pictures/latte.jpg";
+import Tea from "/pictures/tea.jpg";
+
+export const DUMMY_DRINKS = [
+  {
+    id: 1,
+    title: "Latte",
+    image: Latte,
+    price: 90,
+  },
+  {
+    id: 2,
+    title: "Black Tea",
+    image: blackTea,
+    price: 60,
+  },
+  {
+    id: 3,
+    title: "Juice",
+    image: Juice,
+    price: 70,
+  },
+  {
+    id: 4,
+    title: "Tea",
+    image: Tea,
+    price: 70,
+  },
+];
---
+++ src/assets/food.js
@@ -0,0 +1,32 @@
+import Matcha from "/pictures/matcha.jpg";
+import Pancake from "/pictures/pancake.jpg";
+import StrawBerry from "/pictures/strawberry.jpg";
+import CheeseCake from "/pictures/cheeseCake.jpg";
+
+export const FOODS = [
+  {
+    id: 1,
+    title: "Strawberry tart",
+    image: StrawBerry,
+    price: 120,
+  },
+  {
+    id: 2,
+    title: "Matcha Cake",
+    image: Matcha,
+    price: 120,
+  },
+  {
+    id: 3,
+    title: "Pancake ",
+    image: Pancake,
+    price: 150,
+  },
+
+  {
+    id: 4,
+    title: "Cheese Cake ",
+    image: CheeseCake,
+    price: 90,
+  },
+];
