ad1b6a8a6e4155e34505b02d963f445b06c52f24
ritacc12 <rita20920@gmail.com>
2024-03-08 16:12:39 +0800

    import  Activity & Catalog to the main page container

---
+++ src/page/Layout.jsx
@@ -0,0 +1,11 @@
+import Activity from "../component/Activity.jsx";
+import Catalog from "../component/Catalog.jsx";
+
+export default function Layout() {
+  return (
+    <>
+      <Activity />
+      <Catalog />
+    </>
+  );
+}
