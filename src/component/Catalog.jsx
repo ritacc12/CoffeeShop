787779729865972ad4df46cbaeabc61c19576dd5
ritacc12 <rita20920@gmail.com>
2024-03-08 16:11:25 +0800

    create Catalog component for menu and introduction

---
+++ src/component/Catalog.jsx
@@ -0,0 +1,35 @@
+import Card from "react-bootstrap/Card";
+import { Outlet, Link } from "react-router-dom";
+
+export default function Catalog() {
+  return (
+    <>
+      <div className="Catalog">
+        <Card className="catalog-item position: relative">
+          <Card.Img
+            className="catalog-img  d-block"
+            src="/pictures/story.jpg"
+            alt="introduction"
+          ></Card.Img>
+
+          <Card.Body>
+            <Card.Text className="pt-serif-bold-italic">INTRODUCTION</Card.Text>
+            <a href="/connect" className="stretched-link"></a>
+          </Card.Body>
+        </Card>
+
+        <Card className="catalog-item position: relative">
+          <Card.Img
+            className="catalog-img d-block"
+            src="/pictures/menu.jpg"
+            alt="introduction"
+          ></Card.Img>
+          <Card.Body>
+            <Card.Text className="pt-serif-bold-italic">MENU</Card.Text>
+            <a href="/menu" className="stretched-link"></a>
+          </Card.Body>
+        </Card>
+      </div>
+    </>
+  );
+}
