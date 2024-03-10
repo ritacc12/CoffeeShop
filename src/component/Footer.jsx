b3ad87674c7f4b4bac0c002424e7ee445677a2a0
ritacc12 <rita20920@gmail.com>
2024-03-10 15:19:02 +0800

    add the form for validation

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -10,35 +10,41 @@ export default function Footer({ email }) {
             <p>Subscribe and stay on top of our latest news and promotions</p>
           </div>
           <div className="subscribeForm pt-serif-regular">
-            <input
-              className="inputForm"
-              type="Email"
-              value={email}
-              placeholder="Enter your Email"
-            ></input>
-            <button className="subscribeBtn">Subscribe</button>
+            <form>
+              <input
+                className="inputForm"
+                type="email"
+                value={email}
+                placeholder=" Enter your Email"
+              ></input>
+              <button className="subscribeBtn">Subscribe</button>
+            </form>
           </div>
         </div>
         <div className="footer-logo-container">
           <Row>
             <Col>
               <span>
-                <img
-                  src="/pictures/facebook.svg"
-                  width="20"
-                  height="20"
-                  alt="FB"
-                ></img>
+                <a href="https://www.facebook.com/?locale=zh_TW">
+                  <img
+                    src="/pictures/facebook.svg"
+                    width="20"
+                    height="20"
+                    alt="FB"
+                  ></img>
+                </a>
               </span>
             </Col>
             <Col>
               <span>
-                <img
-                  src="/pictures/instagram.svg"
-                  width="20"
-                  height="20"
-                  alt="IG"
-                ></img>
+                <a href="https://www.instagram.com/">
+                  <img
+                    src="/pictures/instagram.svg"
+                    width="20"
+                    height="20"
+                    alt="IG"
+                  ></img>
+                </a>
               </span>
             </Col>
           </Row>
