419874a3dceb1a0ccbd4b2ba701d8a79cc6bb5ae
ritacc12 <rita20920@gmail.com>
2024-03-19 15:54:27 +0800

    import useState to manage the order quantity & add the button group

--- src/component/MealItem.jsx
+++ src/component/MealItem.jsx
@@ -1,11 +1,20 @@
 import Card from "react-bootstrap/Card";
 import { CartContext } from "../store/CartContext.jsx";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import "../styles/Meal.css";
 
-export default function MealItem({ id, title, price, image, onAddToCart }) {
+export default function MealItem({
+  id,
+  title,
+  price,
+  image,
+  onAddToCart,
+  onDecreaseItem,
+}) {
   //const { addItemToCart } = useContext(CartContext);
+  const [quantity, setQuantity] = useState(0);
 
+  const InitQuantity = 0;
   return (
     <>
       <Card
@@ -13,17 +22,39 @@ export default function MealItem({ id, title, price, image, onAddToCart }) {
         style={{
           width: "20rem",
           margin: "10px 20px 10px 20px",
+          padding: "30px",
         }}
         id={id}
       >
         <Card.Img variant="top" src={image} alt={title} className="rounded" />
-        <Card.Body>
-          <Card.Title className="mealTitle">{title}</Card.Title>
-          <Card.Text className="mealPrice">${price}</Card.Text>
+        <Card.Body className="cardBody">
+          <Card.Title className="mealTitle" style={{ fontSize: "25px" }}>
+            {title}
+          </Card.Title>
+          <Card.Text className="mealPrice" style={{ fontSize: "18px" }}>
+            ${price}
+          </Card.Text>
+          <div className="cart-item-actions">
+            <button
+              className="CartBtn"
+              onClick={() =>
+                onDecreaseItem(
+                  id,
+                  setQuantity(quantity <= 0 ? 0 : quantity - 1)
+                )
+              }
+            >
+              -
+            </button>
+            <span className="ItemQuantity">{quantity}</span>
+            <button
+              className="CartBtn"
+              onClick={() => onAddToCart(id, setQuantity(quantity + 1))}
+            >
+              +
+            </button>
+          </div>
         </Card.Body>
-        <button className="CartBtn" onClick={() => onAddToCart(id)}>
-          Add to Cart
-        </button>
       </Card>
     </>
   );
