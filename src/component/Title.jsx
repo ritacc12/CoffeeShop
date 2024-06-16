7b685a5421dc5d89bd83c2b728d65b7479cf965c
ritacc12 <rita20920@gmail.com>
2024-06-16 15:33:44 +0800

    adjust font type & color

--- src/component/Title.jsx
+++ src/component/Title.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Title = ({ title, ...props }) => {
   return (
     <div className="title">
-      <h2 className="News_title" {...props}>
+      <h2 className="News_title pt-serif-bold-italic" {...props}>
         {title}
       </h2>
     </div>
